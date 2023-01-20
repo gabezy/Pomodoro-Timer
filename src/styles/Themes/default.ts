09255c3ecd75d5ea3be84a4cb5a3b173738d5f75
gabezy <gabrielmoreira2705@gmail.com>
2023-01-19 22:12:54 -0300

    💄 add fonts and change global

--- src/styles/Themes/default.ts
+++ src/styles/Themes/default.ts
@@ -1,15 +1,21 @@
 export const defaultTheme = {
-  green300: "#00B37E",
-  green500: "#00875F",
-  green700: "#015F43",
-  red500: "#F03847",
-  red700: "#7A1921",
-  gray100: "#E1E1E6",
-  gray200: "#C4C4CC",
-  gray300: "#8D8D99",
-  gray400: "#7C7C8A",
-  gray500: "#323238",
-  gray600: "#202024",
-  gray700: "#121214",
-  white: "#FFFFFF",
+  white: "#FFF",
+
+  "gray-100": "#E1E1E6",
+  "gray-300": "#C4C4CC",
+  "gray-400": "#8D8D99",
+  "gray-500": "#7C7C8A",
+  "gray-600": "#323238",
+  "gray-700": "#29292E",
+  "gray-800": "#202024",
+  "gray-900": "#121214",
+
+  "green-300": "#00B37E",
+  "green-500": "#00875F",
+  "green-700": "#015F43",
+
+  "red-500": "#AB222E",
+  "red-700": "#7A1921",
+
+  "yellow-500": "#FBA94C",
 };
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,8 +6,18 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box
   }
+:focus {
+  outline: 0;
+  box-shadow: 0 0 0 2px ${({ theme }) => theme["gray-500"]}
+}
+
   body {
-    background: ${({ theme }) => theme.gray700};
-    color: ${({ theme }) => theme.white}
+    background: ${({ theme }) => theme["gray-900"]};
+    color: ${({ theme }) => theme["gray-300"]}
+  }
+  body, input, textarea, button {
+    font-size: 1rem;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400; 
   }
 `;
