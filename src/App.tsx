73d3ade25733acbd7044e107433a0f0554c63e5a
gabezy <gabrielmoreira2705@gmail.com>
2023-01-19 22:36:25 -0300

    ✨ add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
+import { Router } from "./Router";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/Themes/default";
 
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );
